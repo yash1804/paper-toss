6a70d21bde4efd576c6995eedcf6cb02854a3d20
yash1804 <70275059+yash1804@users.noreply.github.com>
2020-11-07 19:42:00 +0530

    Update ball.js

--- ball.js
+++ ball.js
@@ -9,12 +9,16 @@ class Ball {
     this.body = Bodies.circle(x, y, r, options);
     this.r=r;
     World.add(world, this.body);
+    this.image = loadImage("sprites/paper.png");
   }
   display(){
     var pos =this.body.position;
     ellipseMode(RADIUS);
     fill("red");
     ellipse(pos.x, pos.y, this.r, this.r);
+    imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
     
   }
 }
